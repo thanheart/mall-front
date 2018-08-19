5868e3e8585b51d34a63b967da12e0666c8ffcbc
thanheart <332189370@qq.com>
2018-08-19 18:41:48 +0800

    first

---
+++ mmall/src/page/common/nav-simple/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: yun
+* @Date:   2017-05-17 11:26:25
+* @Last Modified by:   yun
+* @Last Modified time: 2017-05-17 11:26:46
+*/
+
+'use strict';
+require('./index.css');
---
+++ mmall/src/util/slider/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: yun
+* @Date:   2017-05-26 18:19:14
+* @Last Modified by:   yun
+* @Last Modified time: 2017-05-26 18:20:00
+*/
+
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
